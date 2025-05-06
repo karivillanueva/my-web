8ea1bc5ee81121c5a641b23cd95a00e937312f0f
karivillanueva <karimevillanueva96@gmail.com>
2025-05-05 21:05:09 -0300

    Create blog list

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,15 +1,21 @@
 import BlogCard from '@/components/blogs/BlogCard';
+import { BLOGS } from '@/utils/constants';
 
 const Blogs = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1>My Blogs</h1>
 
-      <BlogCard
-        title="What is AI & Machine Learning: How it works & more (2024)"
-        image="/images/ai-post.png"
-        link="https://loopstudio.dev/what-is-ai-and-machine-learning/"
-      />
+      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {BLOGS.map((blog) => (
+          <BlogCard
+            key={blog.title}
+            title={blog.title}
+            image={blog.image}
+            link={blog.link}
+          />
+        ))}
+      </div>
     </div>
   );
 };
