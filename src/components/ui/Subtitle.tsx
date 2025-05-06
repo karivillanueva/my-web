446d19a8d04e1eab11c823ed72470036e7b129ac
karivillanueva <karimevillanueva96@gmail.com>
2025-05-05 21:14:51 -0300

    Update titles

---
+++ src/components/ui/Subtitle.tsx
@@ -0,0 +1,7 @@
+const Subtitle = ({ subtitle }: { subtitle: string }) => {
+  return (
+    <h2 className="text-xl font-semibold text-gray-800 mb-2">{subtitle}</h2>
+  );
+};
+
+export default Subtitle;
